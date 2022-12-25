bb16631a3062899281b29f86b56a3430ec14d162
Dipanjan Mondal <mdipanjan33@gmail.com>
2022-12-26 00:49:02 +0530

    Init

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from telegram.ext import Updater, CommandHandler
+import subprocess
+
+
+def start(update, context):
+    update.message.reply_text("""Hello! I am a bot that can monitor the CPU usage of your Linux VPS. 
+    /cpu command to see the current CPU usage.
+    /processes command to see the current running processes.
+    """)
+
+
+def cpu(update, context):
+    usage = os.popen("top -bn1 | grep 'Cpu(s)' | awk '{print $2 + $4}'").read()
+    update.message.reply_text(f'Current CPU usage: {usage}%')
+
+
+def processes(update, context):
+    running_processes = subprocess.check_output(['pgrep', '-f', 'screen'])
+    update.message.reply_text(f'Current processes: {running_processes}%')
+
+
+updater = Updater(TOKEN, use_context=True)
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('cpu', cpu))
+updater.dispatcher.add_handler(CommandHandler('processes', processes))
+
+updater.start_polling()
+updater.idle()
